9c48ffe5c4a9156e4539a0b16b6490a62f82dfda
nicejames <leessju@gmail.com>
2018-12-17 12:33:45 +0900

    Travis config

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ mongoose
     console.log(err);
   });
 
-if (['production'].includes(process.env.NODE_ENV)) {
+if (['production', 'ci'].includes(process.env.NODE_ENV)) {
   app.use(express.static('client/build'));
 
   const path = require('path');
